7b3df621e569989bc92cf7df7fef82f435f78618
Arvind Varma <av894883@gmail.com>
2022-10-21 20:56:56 +0530

    hello

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const GithubActions = require('./GithubActions');
 const bodyParser = require('body-parser')
 const app = express();
 const cors = require("cors");
- require("dotenv").config()
+require("dotenv").config()
 app.use(cors());
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -15,39 +15,59 @@ var router1 = express.Router();
 var router2 = express.Router();
 var router3 = express.Router();
 var router4 = express.Router();
-app.listen(process.env.Port,() => {
+app.listen(process.env.Port, () => {
     console.log(`Server is runing `);
 })
 
 mongoose.connect(process.env.dbUrl).then(() => console.log(`dataBase connection sucessfully`))
-.catch((e) => console.log(`dataBase connection field ${e}`))
+    .catch((e) => console.log(`dataBase connection field ${e}`))
 
-router1.post("/push", (req, res) => {
-    GithubActions.create(req.body)
-    console.log(req.body)
-    res.status(200).json("complrted")
+router1.post("/push", async(req, res) => {
+    const {request_id, author, branch, createdAt} = req.body;
+    await GithubActions.create({
+        request_id,
+        author,
+        action: "push",
+        to_branch: branch,
+        timestamp: createdAt
+    })
+    res.json({message: "success"})
 })
 
-router2.post("/margePR",(req, res) => {
-    GithubActions.create(req.body)
-    console.log(req.body)
-    res.status(200).json("complrted")
+router2.post("/margePR", async(req, res) => {
+    const {request_id, author, from_branch, to_branch, createdAt} = req.body;
+    await GithubActions.create({
+        request_id,
+        author,
+        action: "merge_pr",
+        from_branch,
+        to_branch,
+        timestamp: createdAt
+    })
+    res.json({message: "success"})
 })
 
-router3.post("/pull", (req, res) => {
-    GithubActions.create(req.body)
-    console.log(req.body)
-    res.status(200).json("complrted")
+router3.post("/pull", async(req, res) => {
+    const {request_id, author, from_branch, to_branch, createdAt} = req.body;
+    await GithubActions.create({
+        request_id,
+        author,
+        action: "pull_request",
+        from_branch,
+        to_branch,
+        timestamp: createdAt
+    })
+    res.json({message: "success"})
 })
 
-router4.get("/data", async(req, res) => {
+router4.get("/data", async (req, res) => {
     let data = await GithubActions.find({});
     res.send(data)
 })
 
 
- 
- 
+
+
 app.use(router1);
 app.use(router2);
 app.use(router3);
